86c0588ae50206873ef9607cd20c6f4a2411a0c4
Rusanov Andrey <rusanov.newmail@yandex.ru>
2023-10-06 19:14:56 +0300

    Promo

---
+++ src/components/AboutMain/AboutMain.js
@@ -0,0 +1,11 @@
+import Promo from '../Promo/Promo'
+
+import './AboutMain.css';
+
+export default function AboutMain() {
+  return (
+    <div className='about-main'>
+      <Promo />
+    </div>
+  );
+}
---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import AboutMain from '../AboutMain/AboutMain'
+import './App.css';
+
+function App() {
+  return (
+    <div className='app-container'>
+      <AboutMain/>
+    </div>
+  );
+}
+
+export default App;
