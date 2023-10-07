3b4394a13b7835e1a34ae30be7e6bab089d1462d
Rusanov Andrey <rusanov.newmail@yandex.ru>
2023-10-07 22:21:52 +0300

    Movies начало

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,20 @@
-import AboutMain from '../AboutMain/AboutMain'
+import React from 'react';
+import {BrowserRouter, Route, Routes, Navigate, useNavigate } from 'react-router-dom';
+
+import AboutMain from '../AboutMain/AboutMain';
+import Movies from '../Movies/Movies';
+
 import './App.css';
 
 function App() {
   return (
     <div className='app-container'>
-      <AboutMain/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AboutMain/>} />
+          <Route path="/movies" element={<Movies/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
