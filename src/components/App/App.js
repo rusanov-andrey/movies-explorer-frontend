7efaaae273961e67b0602ff1a3a3d53bac664537
Rusanov Andrey <rusanov.newmail@yandex.ru>
2023-10-15 23:15:11 +0300

    NotFound

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,7 @@ import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
 import Register from '../Register/Register';
 import Login from '../Login/Login';
+import NotFound  from '../NotFound/NotFound';
 
 import './App.css';
 
@@ -21,6 +22,7 @@ function App() {
           <Route path='/profile' element={<Profile/>} />
           <Route path='/signup' element={<Register/>} />
           <Route path='/signin' element={<Login/>} />
+          <Route path='*' element={<NotFound/>} />
         </Routes>
       </BrowserRouter>
     </div>
