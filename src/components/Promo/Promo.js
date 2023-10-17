1448c538c31ea022990b8ea5b80aef80efa1f9bb
Rusanov Andrey <rusanov.newmail@yandex.ru>
2023-10-17 13:40:20 +0300

    :hover

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -7,7 +7,7 @@ export  default function Promo() {
       <div className='promo_info'>
         <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
         <p className='promo__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
-        <div className='promo__button'>Узнать больше</div>
+        <a className='promo__button' href='https://ya.ru/'>Узнать больше</a>
       </div>
     </section>
   );
